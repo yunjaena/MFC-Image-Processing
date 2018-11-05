99ccd0701043f33da510feed0456c6488126217c
yunjaeana <yunjaeana@gmail.com>
2018-11-05 17:42:37 +0900

    add Image Inverse Tool

---
+++ ImageTool_Ch_5/IppEnhance.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include ".\IppImage\IppImage.h"
+
+void IppInverse(IppByteImage& img);
---
+++ ImageTool_Ch_5/IppEnhancement.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "IppEnhance.h"
+
+void IppInverse(IppByteImage& img)
+{
+	int size = img.GetSize();
+	BYTE* p = img.GetPixels();
+
+	for (int i = 0; i < size; i++)
+	{
+		p[i] = 255 - p[i];
+	}
+}
